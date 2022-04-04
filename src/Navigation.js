addd18dbcbbb85411b7b2435427f3edc2b6e1f3d
IrakliChichua <iraklichihcua7@gmail.com>
2022-04-04 19:11:00 +0400

    .

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import {Nav} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 function Navigation() {
     return (
-        <Nav
-            activeKey="/"
-        >
-            <Nav.Item>
-                <Nav.Link as={Link} to="/students">Students</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={Link} to="/teachers">Teachers</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={Link} to="/groups">Groups</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <Navbar collapseOnSelect  bg="dark" variant="dark" style={{ height: "49px" }}>
+            <Container>
+                    <Nav activeKey="/" className="me-auto">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/home">მთავარი</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/home">ჩვენს შესახებ</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/home">სიახლეები</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/home">პარტნიორები</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/home">კონტაქტი</Nav.Link>
+                        </Nav.Item>
+
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <Navbar.Text>ცხელი ხაზი</Navbar.Text>
+                        <Navbar.Text>2 991 991</Navbar.Text>
+                    </Nav>
+            </Container>
+        </Navbar>
     );
 }
-
+/*<Nav
+    activeKey="/"
+>
+    <Nav.Item>
+        <Nav.Link as={Link} to="/students">Students</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link as={Link} to="/teachers">Teachers</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link as={Link} to="/groups">Groups</Nav.Link>
+    </Nav.Item>
+</Nav>*/
 
 export default Navigation;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Alert} from "react-bootstrap";
 import App from "./App";
+import Home from "./Home";
 
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App/>}>
-                <Route index element={<MainPage/>}/>
+                <Route index element={<Home/>}/>
+                <Route path='/home' element={<Home/>}/>
                 <Route path='*' element={<Alert variant="danger"> 404 page not found</Alert>}/>
             </Route>
         </Routes>
