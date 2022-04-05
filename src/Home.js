184a5178bbeff10f9bf8785d9580cce8ef817064
IrakliChichua <iraklichihcua7@gmail.com>
2022-04-05 15:22:29 +0400

    .

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Home(props) {
     return (
-        <div>njnj</div>
+        <div>
+{/*
+            <img src="images/bgi.png" height="1252" width="3840" alt={'bgi'}/>
+*/}         sk
+        </div>
     );
 }
 
