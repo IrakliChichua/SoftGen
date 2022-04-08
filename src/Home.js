1f582460e2207d4132e616b567c70f051df6bab1
IrakliChichua <iraklichihcua7@gmail.com>
2022-04-08 17:46:08 +0400

    .

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import "./home.css"
+import bgi from "./images/bgi.png"
+import dots from "./images/dots.svg"
+import left from "./images/left.png"
+import right from "./images/right.png"
 
 function Home(props) {
     return (
-        <div className={'top-main'}>
-{/*            <div className={'top'}>
+        <div className={"top-main"}>
 
-            </div>
-            <div className={'bot-left'}>
-
-            </div>
-            <div className={'bot-right'}>
-
-            </div>*/}
         </div>
+
+/*
+        <div className="container">
+            <h2>Carousel Example</h2>
+            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"/>
+                    <li data-target="#myCarousel" data-slide-to="1"/>
+                    <li data-target="#myCarousel" data-slide-to="2"/>
+                </ol>
+
+                <div className="carousel-inner">
+
+                    <div className="item active">
+                        <img src={bgi} alt="Los Angeles" style="width:100%;"/>
+                        <div className="carousel-caption">
+                            <h3>Los Angeles</h3>
+                            <p>LA is always so much fun!</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <img src={bgi} alt="Chicago" style="width:100%;"/>
+                        <div className="carousel-caption">
+                            <h3>Chicago</h3>
+                            <p>Thank you, Chicago!</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <img src={bgi} alt="New York" style="width:100%;"/>
+                        <div className="carousel-caption">
+                            <h3>New York</h3>
+                            <p>We love the Big Apple!</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span className="glyphicon glyphicon-chevron-left"/>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span className="glyphicon glyphicon-chevron-right"/>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </div>*/
     );
 }
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import "./nav.css"
+import logo from "./images/logo.svg"
+import shopping_cart from "./images/shopping_cart.svg"
+import profile from "./images/profile.svg"
+import phone from "./images/phone.svg"
 
 function Navigation() {
     return (
@@ -27,17 +31,30 @@ function Navigation() {
             </Navbar>
             <div className={'lower-nav'}>
                 <Nav className={'lower-nav-nav'} activeKey="/">
-                    <Nav.Link as={Link} to="/home">
-                        {/*<img src="images/Logo.svg" alt="logo"/>*/}
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/home">ჩვენს შესახებ</Nav.Link>
-                    <Nav.Link as={Link} to="/home">სიახლეები</Nav.Link>
-                    <Nav.Link as={Link} to="/home">პარტნიორები</Nav.Link>
-                    <Nav.Link as={Link} to="/home" style={{padding: 0}}>კონტაქტი</Nav.Link>
+                    <div className={'left'}>
+                        <Nav.Link as={Link} to="/home">
+                            <img src={logo} alt=""/>
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/home">ფიზიკური<br/>პირებისთვის</Nav.Link>
+                        <Nav.Link as={Link} to="/home">იურიდიული<br/>პირებისთვის</Nav.Link>
+                        <Nav.Link as={Link} to="/home">სავალდებულო<br/>დაზღვევა</Nav.Link>
+                    </div>
+                    <div className={'right'}>
+                        <Nav.Link className={'buy-online-box'} as={Link} to="/home">
+                            <img src={shopping_cart} style={{ marginRight: 8 }} alt=""/>
+                            <div>შეიძინე ონლაინ</div>
+                        </Nav.Link>
+                        <Nav.Link className={'my-office'} as={Link} to="/home" style={{padding: 0, marginRight: 0}}>
+                            <img src={profile} style={{ marginRight: 13 }} alt=""/>
+                            <div>ჩემი<br/>კაბინეტი</div>
+                        </Nav.Link>
+                    </div>
+
                 </Nav>
             </div>
             <div className={'phone'}>
-
+                <img width="30.13" height="30.13" src={phone} alt=""/>
+                <div style={{fontSize: 11}}>აგენტთან<br/>დაკავშირება</div>
             </div>
         </div>
 
